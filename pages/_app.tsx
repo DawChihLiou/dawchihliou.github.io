0c4c4d533ac87b2c418238c56fe5aa685a34a22f
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-09-29 22:24:57 +0200

    feat: apply dark mode styling

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -40,20 +40,10 @@ function MyApp({ Component, pageProps }) {
           sizes="180x180"
           href="/apple-touch-icon.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/favicon-16x16.png"
-        />
+        <link rel="icon" type="image/svg" sizes="32x32" href="/favicon.svg" />
+        <link rel="icon" type="image/svg" sizes="16x16" href="/favicon.svg" />
+        <link rel="mask-icon" href="/favicon.svg" color="#ffffff"></link>
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#007cf0" />
         <meta name="msapplication-TileColor" content="#007cf0" />
         <meta name="theme-color" content="#ffff" />
       </Head>
