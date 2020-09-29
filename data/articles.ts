0fd40769257ce628569a49e3f3bb502ba225e070
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-09-29 10:46:01 +0200

    refactor: update category

--- data/articles.ts
+++ data/articles.ts
@@ -1,7 +1,7 @@
 import socials from '../utils/socials'
 
 const category = {
-  random: 'Random',
+  welcome: 'Welcome',
 }
 
 export default [
@@ -12,7 +12,7 @@ export default [
     description:
       "Thank you so much for being interested in my articles! I'll use this space to write about web technologies. Hopefully it'll unblock you and give you a little dose of inspiration. Stay tuned!",
     url: '/articles/helloworld',
-    category: category.random,
+    category: category.welcome,
     author: 'Daw-Chih Liou',
     avatar: '/portrait.png',
     social: socials.github,
