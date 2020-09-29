10f387c54f3ba694929a2aeb1dea4edc4562b1a0
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-09-29 10:43:03 +0200

    chore: deploy to gh-pages

---
+++ out/_next/static/Z7pOjATHF87psf1Alt2n7/_buildManifest.js
@@ -0,0 +1,31 @@
+self.__BUILD_MANIFEST = {
+  __rewrites: [],
+  '/': [
+    'static\u002Fchunks\u002F6728d85a.b71f61e611268a48c525.js',
+    'static\u002Fcss\u002F69fd9345a27d19104acc.css',
+    'static\u002Fchunks\u002Fpages\u002Findex-ff4f147b6866c142f09f.js',
+  ],
+  '/_error': [
+    'static\u002Fchunks\u002Fpages\u002F_error-b52f8b678ce4b85a362a.js',
+  ],
+  '/articles': [
+    'static\u002Fcss\u002F9aba4da3f39b01bac889.css',
+    'static\u002Fchunks\u002Fpages\u002Farticles-8d69f0f22b8f054b4838.js',
+  ],
+  '/articles/helloworld': [
+    'static\u002Fchunks\u002Fpages\u002Farticles\u002Fhelloworld-1dbc7b667f723e18cc3c.js',
+  ],
+  '/now': [
+    'static\u002Fcss\u002F1c8e07941cd976631080.css',
+    'static\u002Fchunks\u002Fpages\u002Fnow-05d63a0d62b8d663e136.js',
+  ],
+  sortedPages: [
+    '\u002F',
+    '\u002F_app',
+    '\u002F_error',
+    '\u002Farticles',
+    '\u002Farticles\u002Fhelloworld',
+    '\u002Fnow',
+  ],
+}
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -3,15 +3,6 @@ self.__BUILD_MANIFEST = {
   '/': ['static\u002Fchunks\u002Fpages\u002Findex.js'],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
   '/articles': ['static\u002Fchunks\u002Fpages\u002Farticles.js'],
-  '/articles/helloworld': [
-    'static\u002Fchunks\u002Fpages\u002Farticles\u002Fhelloworld.js',
-  ],
-  sortedPages: [
-    '\u002F',
-    '\u002F_app',
-    '\u002F_error',
-    '\u002Farticles',
-    '\u002Farticles\u002Fhelloworld',
-  ],
+  sortedPages: ['\u002F', '\u002F_app', '\u002F_error', '\u002Farticles'],
 }
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
