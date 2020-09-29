8e475b4a7d72820e7139e468b819cb4e162c0670
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-09-29 19:18:59 +0200

    chore: deploy to gh-pages

--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -2,6 +2,18 @@ self.__BUILD_MANIFEST = {
   __rewrites: [],
   '/': ['static\u002Fchunks\u002Fpages\u002Findex.js'],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
-  sortedPages: ['\u002F', '\u002F_app', '\u002F_error'],
+  '/articles': ['static\u002Fchunks\u002Fpages\u002Farticles.js'],
+  '/next/dist/pages/_error': [
+    'static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js',
+  ],
+  '/now': ['static\u002Fchunks\u002Fpages\u002Fnow.js'],
+  sortedPages: [
+    '\u002F',
+    '\u002F_app',
+    '\u002F_error',
+    '\u002Farticles',
+    '\u002Fnext\u002Fdist\u002Fpages\u002F_error',
+    '\u002Fnow',
+  ],
 }
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
