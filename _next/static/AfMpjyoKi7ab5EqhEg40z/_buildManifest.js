4df0237984051a8905048e41d0502c05ce0d07b1
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-09-30 08:10:06 +0200

    chore: deploy to gh-pages

--- _next/static/AfMpjyoKi7ab5EqhEg40z/_buildManifest.js
+++ _next/static/AfMpjyoKi7ab5EqhEg40z/_buildManifest.js
@@ -2,8 +2,8 @@ self.__BUILD_MANIFEST = {
   __rewrites: [],
   '/': [
     'static\u002Fchunks\u002F6728d85a.b71f61e611268a48c525.js',
-    'static\u002Fcss\u002Ff59b17c90af95b99df30.css',
-    'static\u002Fchunks\u002Fpages\u002Findex-f91cd9310eaefd9bc557.js',
+    'static\u002Fcss\u002F568a901d99b0f2c155f6.css',
+    'static\u002Fchunks\u002Fpages\u002Findex-87b17ec4c56752141679.js',
   ],
   '/_error': [
     'static\u002Fchunks\u002Fpages\u002F_error-b52f8b678ce4b85a362a.js',
