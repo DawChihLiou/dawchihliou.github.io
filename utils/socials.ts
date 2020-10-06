d793efc09f884c73c8179bbff8f17dab92ac808f
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-10-06 19:27:55 +0200

    feat: add default open graph meta

--- utils/socials.ts
+++ utils/socials.ts
@@ -4,4 +4,7 @@ export default {
   twitter: 'https://twitter.com/dawchihliou',
   github: 'https://github.com/DawChihLiou',
   home: 'https://dawchihliou.github.io/',
+  heroImage: 'https://dawchihliou.github.io/optimized/social-media.png',
+  description:
+    'Daw-Chih is a software engineer, UX advocate, and mentor who is dedicated to Web engineering. His background in Human Centered Computing has led him to work with startups and public companies across North America, Asia, and Europe. He is passionate about meeting business trajectory with user journey and utilizing engineering architecture and performance monitoring to provide optimal user experience.',
 }
