d61829bf4790fa65f54827fac3c1e82806548149
Daw-Chih Liou <daw-chih.liou@trivago.com>
2020-10-06 16:33:35 +0200

    fix: fix helloworld page layout

--- data/articles.ts
+++ data/articles.ts
@@ -26,7 +26,7 @@ export default [
     description:
       "Thank you so much for being interested in my articles! I'll use this space to write about web technologies. Hopefully it'll unblock you and give you a little dose of inspiration. Stay tuned!",
     url: '/articles/helloworld',
-    cover: '/optimized/portrait-sm.png',
+    cover: '/optimized/portrait.png',
     category: category.welcome,
     author: 'Daw-Chih Liou',
     avatar: '/optimized/portrait-sm.png',
