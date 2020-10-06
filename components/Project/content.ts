1d53088b345a478b1396d8fbb7cbb1147c658b92
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-10-06 06:05:39 +0200

    feat(/lib/webp): optimize images

--- components/Project/content.ts
+++ components/Project/content.ts
@@ -3,16 +3,16 @@ export default [
     title: 'react-use-pip',
     description:
       'reat-use-pip is React hook library for Picture in Picture. It seamlessly detects browser support and provides flexible APIs for developers to have full control over Picture in Picture feature.',
-    image: '/logo-react-use-pip.svg',
-    darkmodeImage: '/logo-react-use-pip-dark.svg',
+    image: '/optimized/logo-react-use-pip.svg',
+    darkmodeImage: '/optimized/logo-react-use-pip-dark.svg',
     link: 'https://dawchihliou.github.io/react-use-pip/',
   },
   {
     title: 'cz-conventional-changelog-with-jiraid-detection',
     description:
       'A Commitizen prompts for conventional changelog standard with Jira ID auto-detection. It introduces an additional step for developers to enter Jira ID. The library detects your Jira ID from the branch name.',
-    image: '/logo-npm.png',
-    darkmodeImage: '/logo-npm.png',
+    image: '/optimized/logo-npm.png',
+    darkmodeImage: '/optimized/logo-npm.png',
     link:
       'https://github.com/DawChihLiou/cz-conventional-changelog-with-jiraid-detection',
   },
@@ -20,8 +20,8 @@ export default [
     title: 'Spes',
     description:
       "Reading turned my life around, and I believe it can turn yours too. There're so many books full of wisdom but it's hard to find yourself a book among the overwhelming options. So I'm giving away a book every month to a subscriber and help you find your next book to read. Sign up! Hopefully you'll be inspired too:)",
-    image: '/logo-spes.png',
-    darmodeImage: '/logo-spes.png',
+    image: '/optimized/logo-spes.png',
+    darmodeImage: '/optimized/logo-spes.png',
     link: 'https://spes.me/',
   },
 ]
