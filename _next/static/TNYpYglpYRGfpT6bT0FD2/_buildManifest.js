9ef3637b1d37ce6768ca3bdb6da0addf8afaa9ac
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-10-06 06:13:40 +0200

    chore: deploy to gh-pages

---
+++ _next/static/TNYpYglpYRGfpT6bT0FD2/_buildManifest.js
@@ -0,0 +1,28 @@
+self.__BUILD_MANIFEST = {
+  __rewrites: [],
+  '/': [
+    'static\u002Fchunks\u002F6728d85a.b71f61e611268a48c525.js',
+    'static\u002Fcss\u002F8c006176896a78f566bb.css',
+    'static\u002Fchunks\u002Fpages\u002Findex-c135be7c5a14cdce3f2c.js',
+  ],
+  '/_error': [
+    'static\u002Fchunks\u002Fpages\u002F_error-b52f8b678ce4b85a362a.js',
+  ],
+  '/articles': [
+    'static\u002Fcss\u002F457d69c08445322f4bbd.css',
+    'static\u002Fchunks\u002Fpages\u002Farticles-5659ef8e1c81fcecd7ac.js',
+  ],
+  '/articles/helloworld': [
+    'static\u002Fchunks\u002Fpages\u002Farticles\u002Fhelloworld-84996b3d19190c7a7ff6.js',
+  ],
+  '/now': ['static\u002Fchunks\u002Fpages\u002Fnow-ac3737acfd6947968fb4.js'],
+  sortedPages: [
+    '\u002F',
+    '\u002F_app',
+    '\u002F_error',
+    '\u002Farticles',
+    '\u002Farticles\u002Fhelloworld',
+    '\u002Fnow',
+  ],
+}
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
--- _next/static/development/_buildManifest.js
+++ _next/static/development/_buildManifest.js
@@ -2,6 +2,22 @@ self.__BUILD_MANIFEST = {
   __rewrites: [],
   '/': ['static\u002Fchunks\u002Fpages\u002Findex.js'],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
-  sortedPages: ['\u002F', '\u002F_app', '\u002F_error'],
+  '/articles': ['static\u002Fchunks\u002Fpages\u002Farticles.js'],
+  '/articles/helloworld': [
+    'static\u002Fchunks\u002Fpages\u002Farticles\u002Fhelloworld.js',
+  ],
+  '/next/dist/pages/_error': [
+    'static\u002Fchunks\u002Fpages\u002Fnext\u002Fdist\u002Fpages\u002F_error.js',
+  ],
+  '/now': ['static\u002Fchunks\u002Fpages\u002Fnow.js'],
+  sortedPages: [
+    '\u002F',
+    '\u002F_app',
+    '\u002F_error',
+    '\u002Farticles',
+    '\u002Farticles\u002Fhelloworld',
+    '\u002Fnext\u002Fdist\u002Fpages\u002F_error',
+    '\u002Fnow',
+  ],
 }
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
