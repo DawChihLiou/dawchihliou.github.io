424aaa43a39175d97bc2601b5bf2e8fca6fd2847
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-10-07 09:11:47 +0200

    chore: deploy to gh-pages

--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -1,15 +1,6 @@
 self.__BUILD_MANIFEST = {
   __rewrites: [],
-  '/': ['static\u002Fchunks\u002Fpages\u002Findex.js'],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
-  '/articles/use-webp-for-better-ux': [
-    'static\u002Fchunks\u002Fpages\u002Farticles\u002Fuse-webp-for-better-ux.js',
-  ],
-  sortedPages: [
-    '\u002F',
-    '\u002F_app',
-    '\u002F_error',
-    '\u002Farticles\u002Fuse-webp-for-better-ux',
-  ],
+  sortedPages: ['\u002F_app', '\u002F_error'],
 }
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
