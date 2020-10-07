7c8eb87b8979175ce8a9ee7b51ce15bc1fe06f90
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-10-07 08:22:22 +0200

    chore: deploy to gh-pages

--- _next/static/K5EdNU8wuWWv-XiZiC2x8/_buildManifest.js
+++ _next/static/K5EdNU8wuWWv-XiZiC2x8/_buildManifest.js
@@ -3,8 +3,8 @@ self.__BUILD_MANIFEST = (function (a, b) {
     __rewrites: [],
     '/': [
       'static\u002Fchunks\u002F6728d85a.9e304be1e48fe7115321.js',
-      'static\u002Fcss\u002F8c006176896a78f566bb.css',
-      'static\u002Fchunks\u002Fpages\u002Findex-996a123dc3e8ab18f12d.js',
+      'static\u002Fcss\u002F06e0d7d1cfa0cf24d3d1.css',
+      'static\u002Fchunks\u002Fpages\u002Findex-bb1be823a2e8a21a99c9.js',
     ],
     '/_error': [
       'static\u002Fchunks\u002Fpages\u002F_error-912df90ed040036b79b4.js',
@@ -16,17 +16,17 @@ self.__BUILD_MANIFEST = (function (a, b) {
     '/articles/helloworld': [
       a,
       b,
-      'static\u002Fchunks\u002Fpages\u002Farticles\u002Fhelloworld-1361744f1ed3238ac8d1.js',
+      'static\u002Fchunks\u002Fpages\u002Farticles\u002Fhelloworld-c1b6df76daef17b945bb.js',
     ],
     '/articles/use-webp-for-better-ux': [
       a,
       b,
-      'static\u002Fchunks\u002Fpages\u002Farticles\u002Fuse-webp-for-better-ux-52640562857f4e5ea70c.js',
+      'static\u002Fchunks\u002Fpages\u002Farticles\u002Fuse-webp-for-better-ux-09f7f29cb9c487bdd4a3.js',
     ],
     '/now': [
       a,
       b,
-      'static\u002Fchunks\u002Fpages\u002Fnow-561e39986dcad7c59a03.js',
+      'static\u002Fchunks\u002Fpages\u002Fnow-fa920ae19f699ae2c41b.js',
     ],
     sortedPages: [
       '\u002F',
@@ -40,6 +40,6 @@ self.__BUILD_MANIFEST = (function (a, b) {
   }
 })(
   'static\u002Fchunks\u002F697f24083cfa071b905bd4b9c5b19f913ba04b05.c73aa5fd2db1e7484028.js',
-  'static\u002Fcss\u002F9451f6dea8c9713e5e76.css'
+  'static\u002Fcss\u002F3edddca397a69c66d733.css'
 )
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
