cb68b0aeec366b6ae878a1bd3c12093e34ecd2f2
Daw-Chih Liou <dawochih.liou@gmail.com>
2020-12-01 20:49:02 +0100

    chore: deploy to gh-pages

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -162,7 +162,7 @@
   }
   /******/
   /******/ var hotApplyOnUpdate = true // eslint-disable-next-line no-unused-vars
-  /******/ /******/ var hotCurrentHash = '4976582296014ad1216c'
+  /******/ /******/ var hotCurrentHash = '4bab8666255aae4d3405'
   /******/ var hotRequestTimeout = 10000
   /******/ var hotCurrentModuleData = {}
   /******/ var hotCurrentChildModule // eslint-disable-next-line no-unused-vars
--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -1,12 +1,14 @@
 self.__BUILD_MANIFEST = {
   __rewrites: [],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
+  '/articles': ['static\u002Fchunks\u002Fpages\u002Farticles.js'],
   '/articles/use-webp-for-better-ux': [
     'static\u002Fchunks\u002Fpages\u002Farticles\u002Fuse-webp-for-better-ux.js',
   ],
   sortedPages: [
     '\u002F_app',
     '\u002F_error',
+    '\u002Farticles',
     '\u002Farticles\u002Fuse-webp-for-better-ux',
   ],
 }
