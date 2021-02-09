30c9e0962fb1fe80254060684c85619e80b9995a
Daw-Chih Liou <dawochih.liou@gmail.com>
2021-02-09 20:37:43 +0100

    chore: deploy to gh-pages

--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -3,6 +3,15 @@ self.__BUILD_MANIFEST = {
   '/': ['static\u002Fchunks\u002Fpages\u002Findex.js'],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
   '/articles': ['static\u002Fchunks\u002Fpages\u002Farticles.js'],
-  sortedPages: ['\u002F', '\u002F_app', '\u002F_error', '\u002Farticles'],
+  '/articles/use-webp-for-better-ux': [
+    'static\u002Fchunks\u002Fpages\u002Farticles\u002Fuse-webp-for-better-ux.js',
+  ],
+  sortedPages: [
+    '\u002F',
+    '\u002F_app',
+    '\u002F_error',
+    '\u002Farticles',
+    '\u002Farticles\u002Fuse-webp-for-better-ux',
+  ],
 }
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
