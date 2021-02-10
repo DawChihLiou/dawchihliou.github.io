1e3b04ba7ee6896fe593d76809204151362dfe3e
Daw-Chih Liou <dawochih.liou@gmail.com>
2021-02-10 17:23:47 +0100

    chore: deploy to gh-pages

--- out/_next/static/development/_buildManifest.js
+++ out/_next/static/development/_buildManifest.js
@@ -2,7 +2,7 @@ self.__BUILD_MANIFEST = {
   __rewrites: [],
   '/': ['static\u002Fchunks\u002Fpages\u002Findex.js'],
   '/_error': ['static\u002Fchunks\u002Fpages\u002F_error.js'],
-  '/now': ['static\u002Fchunks\u002Fpages\u002Fnow.js'],
-  sortedPages: ['\u002F', '\u002F_app', '\u002F_error', '\u002Fnow'],
+  '/articles': ['static\u002Fchunks\u002Fpages\u002Farticles.js'],
+  sortedPages: ['\u002F', '\u002F_app', '\u002F_error', '\u002Farticles'],
 }
 self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
