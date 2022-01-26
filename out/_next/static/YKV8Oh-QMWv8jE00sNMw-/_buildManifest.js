6d813bf0494e4fa255749003d17c9a9d5a22841b
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-01-26 07:54:32 +0100

    chore: deploy to gh-pages

---
+++ out/_next/static/YKV8Oh-QMWv8jE00sNMw-/_buildManifest.js
@@ -0,0 +1,36 @@
+var a, b
+;(b = 'static/chunks/568-f1b6e030898d17cd.js'),
+  (self.__BUILD_MANIFEST = {
+    __rewrites: { beforeFiles: [], afterFiles: [], fallback: [] },
+    '/': [
+      (a = 'static/chunks/ef87329b-36cb7858b125951f.js'),
+      b,
+      'static/chunks/291-b3a9351f339d5b87.js',
+      'static/css/6c11cfd1bcd27c89.css',
+      'static/chunks/pages/index-e8ef4a61525798c3.js',
+    ],
+    '/_error': ['static/chunks/pages/_error-d742f979193aeae4.js'],
+    '/articles': [
+      a,
+      b,
+      'static/css/5be9574e015a7040.css',
+      'static/chunks/pages/articles-b0fddacf1074c1c9.js',
+    ],
+    '/articles/[slug]': [
+      'static/css/c3d1cb2e043c8a7f.css',
+      'static/chunks/pages/articles/[slug]-6c9de093ac362d50.js',
+    ],
+    '/now': [
+      'static/css/0018c274a537ef81.css',
+      'static/chunks/pages/now-d9f6aacb7e4bb2db.js',
+    ],
+    sortedPages: [
+      '/',
+      '/_app',
+      '/_error',
+      '/articles',
+      '/articles/[slug]',
+      '/now',
+    ],
+  }),
+  self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
