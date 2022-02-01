1feb80c72e9099be9e57db5daf12b3c107923781
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-02-01 16:47:41 +0100

    chore: deploy to gh-pages

--- _next/static/chunks/pages/articles/[slug]-26501c0d66d6484e.js
+++ _next/static/chunks/pages/articles/[slug]-26501c0d66d6484e.js
@@ -1,67 +1,13 @@
 ;(self.webpackChunk_N_E = self.webpackChunk_N_E || []).push([
-  [991],
+  [850],
   {
-    7144: function (a, b, c) {
+    5158: function (a, b, c) {
       ;(window.__NEXT_P = window.__NEXT_P || []).push([
-        '/now',
+        '/articles/[slug]',
         function () {
-          return c(9811)
+          return c(2041)
         },
       ])
-    },
-    1208: function (a, b, c) {
-      'use strict'
-      c.d(b, {
-        Z: function () {
-          return h
-        },
-      })
-      var d = c(5893)
-      c(7294)
-      var e = c(1664)
-      function f(a, b, c) {
-        return (
-          b in a
-            ? Object.defineProperty(a, b, {
-                value: c,
-                enumerable: !0,
-                configurable: !0,
-                writable: !0,
-              })
-            : (a[b] = c),
-          a
-        )
-      }
-      function g(a) {
-        for (var b = 1; b < arguments.length; b++) {
-          var c = null != arguments[b] ? arguments[b] : {},
-            d = Object.keys(c)
-          'function' == typeof Object.getOwnPropertySymbols &&
-            (d = d.concat(
-              Object.getOwnPropertySymbols(c).filter(function (a) {
-                return Object.getOwnPropertyDescriptor(c, a).enumerable
-              })
-            )),
-            d.forEach(function (b) {
-              f(a, b, c[b])
-            })
-        }
-        return a
-      }
-      function h(a) {
-        var b = a.href
-        return b && (b.startsWith('/') || b.startsWith('#'))
-          ? (0, d.jsx)(e.default, {
-              href: b,
-              children: (0, d.jsx)('a', g({}, a, { children: a.children })),
-            })
-          : (0, d.jsx)(
-              'a',
-              g({ target: '_blank', rel: 'noopener noreferrer' }, a, {
-                children: a.children,
-              })
-            )
-      }
     },
     420: function (a, b, c) {
       'use strict'
@@ -163,51 +109,158 @@
         })
       }
     },
-    9811: function (a, b, c) {
+    2041: function (a, b, c) {
       'use strict'
       c.r(b),
         c.d(b, {
           __N_SSG: function () {
-            return k
+            return p
           },
           default: function () {
-            return l
+            return q
           },
         })
       var d = c(5893)
       c(7294)
       var e = c(544),
         f = c(420),
-        g = c(3295),
-        h = c(2985),
-        i = c(6944),
-        j = c.n(i),
-        k = !0
-      function l(a) {
-        var b = a.meta,
-          c = (0, e.z)(b.body.code)
-        return (0, d.jsx)(g.Z, {
-          title: "Daw-Chih's ".concat(b.title),
-          description: b.description,
-          url: ''.concat(h.Z.home, '/').concat(b.slug),
-          image: b.cover,
-          imageWidth: b.coverWidth,
-          imageHeight: b.coverHeight,
-          children: (0, d.jsx)('main', {
-            className: j().main,
-            children: (0, d.jsxs)('article', {
-              className: j().article,
+        g = c(7747),
+        h = c.n(g),
+        i = c(2985),
+        j = c(9754),
+        k = c.n(j)
+      function l() {
+        return (0, d.jsxs)('article', {
+          className: k().wrap,
+          children: [
+            (0, d.jsx)('img', {
+              src: '/optimized/raw/portrait-sm.png',
+              alt: 'Daw-Chih Liou',
+              className: k().avatar,
+              loading: 'lazy',
+            }),
+            (0, d.jsxs)('div', {
               children: [
-                (0, d.jsx)('h1', { children: b.title }),
-                (0, d.jsx)(c, { components: f.Z }),
+                (0, d.jsx)('a', {
+                  href: i.Z.home,
+                  className: k().author,
+                  children: (0, d.jsx)('span', { children: 'Daw-Chih Liou' }),
+                }),
+                (0, d.jsx)('p', { children: i.Z.description }),
               ],
+            }),
+          ],
+        })
+      }
+      var m = c(3295)
+      function n(a) {
+        var b = a.children,
+          c = a.title,
+          e = a.description,
+          f = a.publishedAt,
+          g = a.slug,
+          j = a.cover,
+          k = void 0 === j ? '' : j,
+          n = a.coverWidth,
+          o = a.coverHeight,
+          p = a.readingTime,
+          q = a.hideAuthor
+        return (0, d.jsx)(m.Z, {
+          title: c,
+          description: e,
+          url: 'https://dawchihliou.github.io/articles/'.concat(
+            void 0 === g ? '' : g
+          ),
+          image: ''.concat(i.Z.home).concat(k),
+          imageWidth: n,
+          imageHeight: o,
+          publishedAt: f,
+          children: (0, d.jsx)('article', {
+            children: (0, d.jsx)('div', {
+              className: h().container,
+              children: (0, d.jsxs)('div', {
+                className: h().wrap,
+                children: [
+                  (0, d.jsx)('h1', { children: c }),
+                  (0, d.jsxs)('div', {
+                    className: h().info,
+                    children: [
+                      (0, d.jsxs)('div', {
+                        className: h().author,
+                        children: [
+                          (0, d.jsx)('img', {
+                            alt: 'Daw-Chih Liou',
+                            src: '/optimized/raw/portrait-sm.webp',
+                            loading: 'lazy',
+                          }),
+                          (0, d.jsx)('p', { children: 'Daw-Chih Liou' }),
+                        ],
+                      }),
+                      (0, d.jsxs)('p', {
+                        children: [f, p ? ' \xb7 '.concat(p.text) : ''],
+                      }),
+                    ],
+                  }),
+                  (0, d.jsx)('img', { alt: c, src: k, width: '100%' }),
+                  (0, d.jsx)('div', {
+                    className: 'article-content',
+                    children: b,
+                  }),
+                  !q && (0, d.jsx)(l, {}),
+                ],
+              }),
             }),
           }),
         })
       }
+      function o(a, b, c) {
+        return (
+          b in a
+            ? Object.defineProperty(a, b, {
+                value: c,
+                enumerable: !0,
+                configurable: !0,
+                writable: !0,
+              })
+            : (a[b] = c),
+          a
+        )
+      }
+      var p = !0
+      function q(a) {
+        var b = a.post,
+          c = (0, e.z)(b.body.code)
+        return (0, d.jsx)(
+          n,
+          (function (a) {
+            for (var b = 1; b < arguments.length; b++) {
+              var c = null != arguments[b] ? arguments[b] : {},
+                d = Object.keys(c)
+              'function' == typeof Object.getOwnPropertySymbols &&
+                (d = d.concat(
+                  Object.getOwnPropertySymbols(c).filter(function (a) {
+                    return Object.getOwnPropertyDescriptor(c, a).enumerable
+                  })
+                )),
+                d.forEach(function (b) {
+                  o(a, b, c[b])
+                })
+            }
+            return a
+          })({}, b, { children: (0, d.jsx)(c, { components: f.Z }) })
+        )
+      }
     },
-    6944: function (a) {
-      a.exports = { main: 'Now_main__vu_tz', article: 'Now_article__DX6t0' }
+    7747: function (a) {
+      a.exports = {
+        container: 'Article_container__q__L3',
+        wrap: 'Article_wrap__LZypL',
+        info: 'Article_info__bOKys',
+        author: 'Article_author__grS8U',
+      }
+    },
+    9754: function (a) {
+      a.exports = { wrap: 'Author_wrap__s8DeE', avatar: 'Author_avatar__j6KIn' }
     },
     1163: function (a, b, c) {
       a.exports = c(387)
@@ -238,7 +291,7 @@
   },
   function (a) {
     a.O(0, [774, 888, 179], function () {
-      return a((a.s = 7144))
+      return a((a.s = 5158))
     }),
       (_N_E = a.O())
   },
