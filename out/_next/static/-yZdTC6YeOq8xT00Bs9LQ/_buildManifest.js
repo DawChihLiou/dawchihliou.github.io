49abcc759d729598677f0d9728fb2648d1550958
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-02-07 16:59:07 +0100

    chore: deploy to gh-pages

--- out/_next/static/-yZdTC6YeOq8xT00Bs9LQ/_buildManifest.js
+++ out/_next/static/-yZdTC6YeOq8xT00Bs9LQ/_buildManifest.js
@@ -35,6 +35,6 @@
   }
 })(
   'static/chunks/ef87329b-e04d9365c7286ed1.js',
-  'static/chunks/169-37deb4e99bf805ce.js'
+  'static/chunks/169-88079b83f413a916.js'
 )),
   self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
