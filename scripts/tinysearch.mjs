bd9fbf131e6a399f01658154e82fe955af37d39d
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-02-11 12:02:50 +0100

    feat: add script for tinysearch index generation

---
+++ scripts/tinysearch.mjs
@@ -0,0 +1,59 @@
+import prettier from 'prettier'
+import { writeFileSync, mkdirSync, rmSync, existsSync } from 'fs'
+// import spawn from 'cross-spawn'
+import { remark } from 'remark'
+import strip from 'strip-markdown'
+import { allArticles } from '.contentlayer/data'
+import externalArticles from '../.generated/meta/externalArticles.mjs'
+
+const outdir = '.generated/tinysearch'
+const outfilename = 'index.json'
+
+async function generate() {
+  if (existsSync(outdir)) {
+    rmSync(outdir, { recursive: true })
+  }
+
+  const originals = allArticles.map((a) => ({
+    title: a.title,
+    url: `/articles/${a.slug}`,
+    body: a.body.raw,
+  }))
+  const externals = externalArticles.map((a) => ({
+    title: a.title,
+    url: a.url,
+    body: a.description,
+  }))
+  const articles = originals.concat(externals)
+
+  Promise.all(
+    articles.map(async (a) => {
+      const file = await remark().use(strip).process(a.body)
+      return {
+        ...a,
+        body: String(file),
+      }
+    })
+  )
+
+  const prettierConfig = await prettier.resolveConfig('.prettierrc')
+
+  const formatted = prettier.format(JSON.stringify(articles), {
+    ...prettierConfig,
+    parser: 'json',
+  })
+
+  mkdirSync(outdir, { recursive: true })
+
+  writeFileSync(`${outdir}/${outfilename}`, formatted)
+
+  // const { status } = spawn.sync('tinysearch', [`${outdir}/${outfilename}`], {
+  //   stdio: 'inherit',
+  // })
+
+  // if (status !== 0) {
+  //   console.error('tinysearch failed.')
+  // }
+}
+
+generate()
