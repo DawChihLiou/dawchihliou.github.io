c03e3737a571e558ee39de7c403c7173656344a2
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-02-14 17:59:02 +0100

    refactor: remove redundant module
    GH-43

--- utils/date.ts
+++
@@ -1,8 +0,0 @@
-export function format(iso: string) {
-  const options = {
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-  }
-  return new Date(iso).toLocaleDateString('en-US', options)
-}
