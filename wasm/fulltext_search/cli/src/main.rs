1781623d8e1e49676eaf094e33fb1c5ac3ef84f4
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-03-11 19:09:24 +0100

    feat: add cli main initial logic

--- wasm/fulltext_search/cli/src/main.rs
+++ wasm/fulltext_search/cli/src/main.rs
@@ -1,7 +1,16 @@
+use anyhow::{Context, Result};
+use std::fs::read_to_string;
+
 mod filter;
 mod post;
 mod storage;
 
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<()> {
+    let path = String::from("../../../.generated/tinysearch/index.json");
+    let posts: Vec<post::Post> = post::read(
+        read_to_string(&path).with_context(|| format!("directory {:?} not found", &path))?,
+    )?;
+    storage::write(posts, String::from("../storage"))?;
+
+    Ok(())
 }
