7853227563aeec0a8fa2f271dee9212842798ccd
Daw-Chih Liou <dawochih.liou@gmail.com>
2022-03-28 18:58:08 +0200

    feat: imcomplete draft

--- next.config.js
+++ next.config.js
@@ -1,36 +1,42 @@
 const { withContentlayer } = require('next-contentlayer')
-
-module.exports = withContentlayer()({
-  // for deployment to github pages
-  assetPrefix: '',
-
-  // opt-in to SWC's JavaScript minification instead of Terser
-  swcMinify: true,
-
-  webpack: function (config, { isServer }) {
-    config.module.rules.push({
-      test: /\.md$/,
-      use: 'raw-loader',
-    })
-
-    config.module.rules.push({
-      test: /\.svg$/i,
-      issuer: /\.[jt]sx?$/,
-      use: ['@svgr/webpack'],
-    })
-
-    // wasm support workaround
-    // https://github.com/vercel/next.js/issues/25852
-    if (isServer) {
-      config.output.webassemblyModuleFilename =
-        './../static/wasm/[modulehash].wasm'
-    } else {
-      config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm'
-    }
-
-    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
-    config.experiments = { asyncWebAssembly: true }
-
-    return config
-  },
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
 })
+
+module.exports = withContentlayer()(
+  withBundleAnalyzer({
+    // for deployment to github pages
+    assetPrefix: '',
+
+    // opt-in to SWC's JavaScript minification instead of Terser
+    swcMinify: true,
+
+    webpack: function (config, { isServer }) {
+      config.module.rules.push({
+        test: /\.md$/,
+        use: 'raw-loader',
+      })
+
+      config.module.rules.push({
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ['@svgr/webpack'],
+      })
+
+      // wasm support workaround
+      // https://github.com/vercel/next.js/issues/25852
+      if (isServer) {
+        config.output.webassemblyModuleFilename =
+          './../static/wasm/[modulehash].wasm'
+      } else {
+        config.output.webassemblyModuleFilename =
+          'static/wasm/[modulehash].wasm'
+      }
+
+      // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+      config.experiments = { asyncWebAssembly: true }
+
+      return config
+    },
+  })
+)
